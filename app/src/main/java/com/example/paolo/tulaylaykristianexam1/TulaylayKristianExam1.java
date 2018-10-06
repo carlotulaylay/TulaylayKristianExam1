574cedd4fc4dea40ad241fc9d76c4f011a61d277
carlotulaylay <kristiantulaylay@gmail.com>
2018-10-06 18:22:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/paolo/tulaylaykristianexam1/TulaylayKristianExam1.java
@@ -0,0 +1,13 @@
+package com.example.paolo.tulaylaykristianexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TulaylayKristianExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tulaylay_kristian_exam1);
+    }
+}
